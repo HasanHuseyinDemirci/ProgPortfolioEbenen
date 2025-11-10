f1ce5446a6fa0b02333b635ecd2dae1692f53952
HasanHuseyinDemirci <hasanhuseyindemirci@outlook.de>
2025-11-10 11:14:23 +0100

    Update Ebenen.py

--- Ebenen.py
+++ Ebenen.py
@@ -1,18 +1,46 @@
+import random as rnd
 class Ebene:
-    def __init__(self,x,y,z):
+    def __init__(self,x,y,z,b):
         self.x = x
         self.y = y
         self.z = z
-    def __add__(self,summand2):
+        self.b = b
+    def __add__(self,summand):
         result = Ebene(
-            self.x + summand2.x,
-            self.y + summand2.y,
-            self.z + summand2.z
-        )
-    def __multiply__(self,summand2):
-        result = Ebene(
-            self.x + summand2.x,
-            self.y + summand2.y,
-            self.z + summand2.z
+            self.x + summand.x,
+            self.y + summand.y,
+            self.z + summand.z,
+            self.b + summand.b
         )
         return result
+    def __mul__(self,factor):
+        result = Ebene(
+            self.x * factor,
+            self.y * factor,
+            self.z * factor,
+            self.b * factor
+        )
+        return result
+## AB HIER BEGINNT BERECHNUNG (HASAN)
+ebene1 = Ebene(rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10))
+ebene2 = Ebene(rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10))
+print(ebene1.x,ebene1.y,ebene1.z, "|",ebene1.b)
+print(ebene2.x,ebene2.y,ebene2.z, "|",ebene2.b)
+print("--------------")
+def gauss(e1, e2):
+    if e1.x < 0:
+        e1 = e1*(-1)
+        print("e1<0")
+    if e2.x < 0:
+        e2 = e2*(-1)
+        print("e2<0")
+    if e1.x != e2.x:
+        e1= e1*(-e2.x/e1.x)
+        e2= e2+e1
+    return e1, e2
+
+ebene1, ebene2 = gauss(ebene1, ebene2)
+print(ebene1.x,ebene1.y,ebene1.z, "|",ebene1.b)
+print(ebene2.x,ebene2.y,ebene2.z, "|",ebene2.b)
+
+## AB HIER ENDET DIE BERECHNUNG (HASAN)
