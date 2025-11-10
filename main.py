3069869709d4779bbddf7d4a39a1caa0ee08f3c4
Vitalii <105307395+Sulphur1407@users.noreply.github.com>
2025-11-10 11:35:06 +0100

    Output v1.0

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import Ebene
 
 
 NAME_INPUT_FILE = ""
-NAME_OF_OUTPUT = ""
+NAME_OUTPUT_FILE = "output.txt"
 DECIMAL_PLACCES = 3
 
 def input():
@@ -13,7 +13,23 @@ def calc_gauss(e1, e2, vis_calc, file_save):
     pass
 #return int: ind, str: equation, str: calc_steps, boll: file_save
 
-def output(ind, gleichung, calc_steps, file_save):
-    pass
-#return None
+def output(ind, equation, calc_steps, file_save):
+    if calc_steps != "":
+        print("Rechenschritte:\n\n" + calc_steps + "\n")
+    
+    if ind == 0:
+        print("Die beiden Ebenen sind echt parallel.")
+    elif ind == 1:
+        print("Die beiden Ebenen sind identisch.")
+    elif ind == 2:
+        print("Die Schnittmenge der beiden Ebenen lautet: " + equation)
+    else:
+        print("Fehler: Indikator muss 0,1 oder 2 sein.")
+
+    if file_save:
+        try:
+            with open(NAME_OUTPUT_FILE, "w") as output:
+                output.write("Die Schnittmenge der beiden Ebenen lautet:" + equation)
+        except:
+            print(f"Fehler: {NAME_OUTPUT_FILE} nicht gefunden.")
 
