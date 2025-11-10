1ec65a61de3c177fc5bc3760912ac9d92e6d2913
HasanHuseyinDemirci <hasanhuseyindemirci@outlook.de>
2025-11-10 15:49:07 +0100

    Update Ebenen.py

--- Ebenen.py
+++ Ebenen.py
@@ -21,7 +21,7 @@ class Ebene:
             self.b * factor
         )
         return result
-## AB HIER BEGINNT BERECHNUNG (HASAN)
+## AB HIER BEGINNT BERECHNUNG (HASAN) 0DIVISION PROBLEM
 ebene1 = Ebene(rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10))
 ebene2 = Ebene(rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10),rnd.randint(-10,10))
 print(ebene1.x,ebene1.y,ebene1.z, "|",ebene1.b)
@@ -30,17 +30,27 @@ print("--------------")
 def gauss(e1, e2):
     if e1.x < 0:
         e1 = e1*(-1)
-        print("e1<0")
     if e2.x < 0:
         e2 = e2*(-1)
-        print("e2<0")
-    if e1.x != e2.x:
-        e1= e1*(-e2.x/e1.x)
-        e2= e2+e1
+
+    e1= e1*(-e2.x/e1.x)
+    e2= e2+e1
+    print(e1.x, e1.y, e1.z, "|", e1.b)
+    print(e2.x, e2.y, e2.z, "|", e2.b)
+    print("--------------")
+
+    if e1.y < 0:
+        e1 = e1*(-1)
+    if e2.y < 0:
+        e2 = e2*(-1)
+    e2= e2*(-e1.y/e2.y)
+    e1= e1+e2
+    print(e1.x, e1.y, e1.z, "|", e1.b)
+    print(e2.x, e2.y, e2.z, "|", e2.b)
+    print("--------------")
     return e1, e2
 
 ebene1, ebene2 = gauss(ebene1, ebene2)
-print(ebene1.x,ebene1.y,ebene1.z, "|",ebene1.b)
-print(ebene2.x,ebene2.y,ebene2.z, "|",ebene2.b)
+
 
 ## AB HIER ENDET DIE BERECHNUNG (HASAN)
