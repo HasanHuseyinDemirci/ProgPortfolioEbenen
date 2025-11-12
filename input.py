9a8524b6fccff8c7e78419f1b8b2e17674f42dd9
andreashubertus <andreas2006hubertus@gmail.com>
2025-11-12 20:45:01 +0100

    Update input.py
    changes in names,csv handeling and edge cases

--- input.py
+++ input.py
@@ -1,38 +1,41 @@
 import csv
 
 save_calculation_steps = None
-answer_yes = ["y", "yes", "ye", "yeah", "ja", "j"]
-answer_no = ["n", "no", "nope", "nein", "never"]
+ANSWER_YES = ("y", "yes","ja","j")
+ANSWER_NO = ("n", "no", "nein")
 
-def eingabe_ebene():
-    global save_calculation_steps, answer_yes, answer_no
+def input_plane_terminal():
+    global ANSWER_YES, ANSWER_NO
     while True:
-        list_ebene = [0, 0, 0, 0]
-        list_ebene_index = ["x", "y", "z", "d"]
-        for i in range(len(list_ebene)):
+        list_plane = [0, 0, 0, 0]
+        list_plane_index = ["x", "y", "z", "d"]
+        for i in range(len(list_plane)):
             while True:
-                list_ebene[i] = input(f"Bitte gib einen gültigen Wert für {list_ebene_index[i]} an ")
+                list_plane[i] = input(f"Bitte gib einen gültigen Wert für {list_plane_index[i]} an ")
                 try:
-                    list_ebene[i] = float(list_ebene[i])
-                    break
+                    if list_plane[i] == "" or list_plane[i] is None or list_plane[i] == "inf" or list_plane[i] == "-inf":
+                        print("Bitte erneut versuchen!")
+                        list_plane[i] = 0
+                        continue
+                    else:
+                        list_plane[i] = float(list_plane[i])
+                        break
                 except ValueError:
                     print("Bitte erneut versuchen!")
-                    list_ebene[i] = list_ebene_index[i]
+                    list_plane[i] = list_plane_index[i]
 
-        frage_nach_ebene = input(f"""
+        ask_is_plane_correct = input(f"""
 ist dies Ebene? (j/n) 
-    {list_ebene[0]} x1 {list_ebene[1]} x2 {list_ebene[2]} x3 - {list_ebene[3]} = 0""" )
-        if frage_nach_ebene[0].lower() == "j":
-            return ({"x": list_ebene[0],"y": list_ebene[1],"z": list_ebene[2],"d": list_ebene[3]})
-            break
-                
-        else:
+    {list_plane[0]} x1 {list_plane[1]} x2 {list_plane[2]} x3 - {list_plane[3]} = 0""" )
+        if ask_is_plane_correct.lower() == None or ask_is_plane_correct.lower() not in ANSWER_YES + ANSWER_NO:
             print("Versuche erneut")
-def main():
-    index_unallowed = []
-    global save_calculation_steps
-    while True:
-        print(
+            continue
+        elif ask_is_plane_correct.lower() == "j":
+            return ({"x": list_plane[0],"y": list_plane[1],"z": list_plane[2],"d": list_plane[3]})
+            break
+
+def start_plane_calculator():
+    print(
             """
     Willkommen beim Ebenenrechner!
         
@@ -40,63 +43,101 @@ def main():
     Du kannst die Ebenenkoeffizienten entweder über eine CSV-Datei einlesen oder direkt in der Kommandozeile eingeben.  
     Anschließend wird die Schnittmenge berechnet, und du kannst wählen, ob das Ergebnis als CSV-Datei exportiert werden soll oder direkt in der Kommandozeile ausgegeben wird.
             """)
-        
 
-        while True:
-            answer = input("Willst du die Rechenschritte Speichern? (j/n) ")
-            if answer.lower() in answer_yes:
-                save_calculation_steps = True
-                break
-            elif answer.lower() in answer_no:
-                save_calculation_steps = False
-                break
-            print("""
-    Bitte erneut versuchen!
-    Dies ist nicht Gültig bitte versuche es mit Ja oder mit Nein
-            """)
+def ask_user_calculation_steps_preferences():
+    global save_calculation_steps, ANSWER_YES, ANSWER_NO
+    while True:
+        answer = input("Willst du die Rechenschritte Speichern? (j/n) ")
+        if answer.lower() in ANSWER_YES:
+            save_calculation_steps = True
+            break
+        elif answer.lower() in ANSWER_NO:
+            save_calculation_steps = False
+            break
+        print("""
+        Bitte erneut versuchen!
+        Dies ist nicht Gültig bitte versuche es mit Ja oder mit Nein
+                """)
+    return save_calculation_steps
+
+def input_plane_csv():
+    index_unallowed = []
+    global ANSWER_YES, ANSWER_NO
+
+    while True:
+        try:
+            with open("ebenen.csv", mode="r", encoding="utf-8") as csvfile:
+                reader = list(csv.reader(csvfile))
+                if len(reader) == 0:
+                    print("Die CSV-Datei ist leer.")
+                    return None, None
+                for index, row in enumerate(reader):
+                    if len(row) == 4:
+                        try:
+                            [float(cell) for cell in row]
+                        except ValueError:
+                            print(index+1, f"Zeile {index + 1} in der CSV-Datei ist ungültig und wird übersprungen.")
+                            index_unallowed.append(index + 1)
+                            continue
+                        print(index+1,row)
+                    else:
+                        print(index+1, f"Zeile {index + 1} in der CSV-Datei ist ungültig und wird übersprungen.")
+                        index_unallowed.append(index + 1)
+
+                while True:
+                    input_choice_of_plane =  input("Bitte wähle zwei Ebenen durch ihre Nummern aus, getrennt durch ein Komma: ").split(",")
+                    if (len(input_choice_of_plane) != 2 or not input_choice_of_plane[0].strip().isdigit() or not input_choice_of_plane[1].strip().isdigit()):
+                        print("Bitte genau zwei Ebenennummern angeben.")
+                        continue
+                                
+                    input_plane_csv_1 = int(input_choice_of_plane[0].strip())
+                    input_plane_csv_2 = int(input_choice_of_plane[1].strip())
+
+                    total_rows = len(reader)      
+
+                    if (
+                            1 <= input_plane_csv_1 <= total_rows and
+                            1 <= input_plane_csv_2 <= total_rows and
+                            input_plane_csv_1 not in index_unallowed and
+                            input_plane_csv_2 not in index_unallowed
+                        ):
+
+                        plane_check = input("Du hast die Ebenen " + str(input_plane_csv_1) + " und " + str(input_plane_csv_2) + " ausgewählt. Ist das korrekt? (j/n) ")
+                        if plane_check.lower() in ANSWER_YES:
+                            row1 = reader[input_plane_csv_1 - 1]
+                            row2 = reader[input_plane_csv_2 - 1]
+                            e1 = {"x": float(row1[0]), "y": float(row1[1]), "z": float(row1[2]), "d": float(row1[3])}
+                            e2 = {"x": float(row2[0]), "y": float(row2[1]), "z": float(row2[2]), "d": float(row2[3])}
+                            return e1, e2
+                        else:
+                            print("Bitte erneut versuchen.")
+                            continue
+        except FileNotFoundError:
+            print("Die Datei 'ebenen.csv' wurde nicht gefunden. Bitte stelle sicher, dass sie im gleichen Verzeichnis wie dieses Programm liegt.")
+            return None, None
+
+def main_input():
+    global save_calculation_steps
+    while True:
+        start_plane_calculator()
+        save_calculation_steps = ask_user_calculation_steps_preferences()
+
         while True:
             file_or_terminal = input("Bevor wir beginnen, willst du deine ebenen im Terminal eingeben oder aus einer CSV Datei auslesen? (T/CSV) ")
             if file_or_terminal.upper() == "T":
                 print("Beginnen wir mit der ersten Ebene:")
-                e1 = eingabe_ebene()
+                e1 = input_plane_terminal()
                 print("Jetzt die zweite Ebene:")
-                e2 = eingabe_ebene()
+                e2 = input_plane_terminal()
                 print(e1,e2)
                 
                 break
             elif file_or_terminal.upper() == "CSV":
-                while True:
-                    with open("ebenen.csv", mode="r", encoding="utf-8") as csvfile:
-                        reader = csv.reader(csvfile)
-                        for index, row in enumerate(reader):
-                            if len(row) == 4:
-                                print(index+1,row)
-                            else:
-                                print(index, f"Zeile {index + 1} in der CSV-Datei ist ungültig und wird übersprungen.")
-                                index_unallowed.append(index + 1)
-
-                        while True:
-                            eingabe_ebenen =  input("Bitte wähle zwei Ebenen durch ihre Nummern aus, getrennt durch ein Komma: ").split(",")
-                            if len(eingabe_ebenen) != 2:
-                                print("Bitte genau zwei Ebenennummern angeben.")
-                                continue
-                            
-                            try:
-                                eingabe_ebene1 = int(eingabe_ebenen[0].strip())
-                                eingabe_ebene2 = int(eingabe_ebenen[1].strip())
-                                if 1 <= eingabe_ebene1 <= index + 1 and 1 <= eingabe_ebene2 <= index + 1 and eingabe_ebene1 not in index_unallowed and eingabe_ebene2 not in index_unallowed:
-                                    break
-                                else:
-                                    print("Ungültige Ebenennummern. Bitte erneut versuchen.")
-                            except ValueError:
-                                print("Ungültige Eingabe. Bitte gib zwei Zahlen ein, getrennt durch ein Komma.")    
-                        ebenenueberpruefung = input(f"Du hast Ebene {eingabe_ebene1} und Ebene {eingabe_ebene2} ausgewählt. Passt das? (j/n) ")
-                        if ebenenueberpruefung in answer_yes:
-                            e1 = {}
-                            e2 = {}
-                            break
-
+                e1, e2 = input_plane_csv()
+                if e1 is None or e2 is None:
+                    continue
                 break
         break
     return e1, e2, save_calculation_steps
-main()
+
+input_plane_csv()
