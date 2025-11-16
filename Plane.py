c77f3500d966fe55890f651324d50df8b4be81f6
Vitalii <105307395+Sulphur1407@users.noreply.github.com>
2025-11-16 21:50:34 +0100

    def __str__(self):     return (         f"{self.a:g}·x "         f"{self.b:+g}·y "         f"{self.c:+g}·z = "         f"{self.d:g}"     )

--- Plane.py
+++ Plane.py
@@ -6,30 +6,14 @@ class Plane:
         self.c = c
         self.d = d
 
-    def __add__(self, other):
-        # Zeilenoperation: R1 + R2
-        return Plane(
-            self.a + other.a,
-            self.b + other.b,
-            self.c + other.c,
-            self.d + other.d
-        )
-
-    def __mul__(self, faktor):
-        # Zeilenoperation: k · R
-        return Plane(
-            self.a * faktor,
-            self.b * faktor,
-            self.c * faktor,
-            self.d * faktor
-        )
-
-    __rmul__ = __mul__  # ermöglicht: k · Ebene
-
     def as_list(self):
         # Rückgabe der Koeffizienten als Tupel (für Gauss-Berechnung)
         return [self.a, self.b, self.c, self.d]
 
     def __str__(self):
-        # Formatierte Ausgabe der Ebenengleichung für die Konsole
-        return f"{self.a}x + {self.b}y + {self.c}z = {self.d}"
+        return (
+            f"{self.a:g}·x "
+            f"{self.b:+g}·y "
+            f"{self.c:+g}·z = "
+            f"{self.d:g}"
+        )
